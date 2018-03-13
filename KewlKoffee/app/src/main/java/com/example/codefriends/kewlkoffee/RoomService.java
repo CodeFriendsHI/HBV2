4273fa0fedef5ab84aed25704ad4361a4d373bd1
adalsteinn95 <adalsteinn95@gmail.com>
2018-03-13 10:45:24 +0000

    service

---
+++ KewlKoffee/app/src/main/java/com/example/codefriends/kewlkoffee/RoomService.java
@@ -0,0 +1,23 @@
+package com.example.codefriends.kewlkoffee;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
+/**
+ * Created by adalsteinn95 on 13.3.2018.
+ */
+
+public interface RoomService {
+
+
+
+    @GET("/{id}")
+    Call <ResponseBody> getStuff(@Path("id") int id);
+
+
+}
