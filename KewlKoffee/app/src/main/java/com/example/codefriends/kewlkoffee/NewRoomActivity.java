88287e9ed1e542fde78e2c64afd5d91032af596e
geirgardarsson <geg42@hi.is>
2018-03-15 10:00:51 +0000

    added layout for new room

---
+++ KewlKoffee/app/src/main/java/com/example/codefriends/kewlkoffee/NewRoomActivity.java
@@ -0,0 +1,24 @@
+package com.example.codefriends.kewlkoffee;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class NewRoomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_newroom);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show());
+    }
+
+}
