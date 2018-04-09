698852fe2c8a2b2ea09423ba0d25aba87a294bf4
DanniGudna <dannigudna@gmail.com>
2018-04-09 13:51:38 +0000

    notifaction service komið, á eftir ða laga hvað takkar gera

--- KewlKoffee/app/src/main/java/com/example/codefriends/kewlkoffee/MyFirebaseMessagingService.java
+++ KewlKoffee/app/src/main/java/com/example/codefriends/kewlkoffee/MyFirebaseMessagingService.java
@@ -37,18 +37,8 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
      *
      * @param remoteMessage Object representing the message received from Firebase Cloud Messaging.
      */
-// [START receive_message]
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        // [START_EXCLUDE]
-        // There are two types of messages data messages and notification messages. Data messages are handled
-        // here in onMessageReceived whether the app is in the foreground or background. Data messages are the type
-        // traditionally used with GCM. Notification messages are only received here in onMessageReceived when the app
-        // is in the foreground. When the app is in the background an automatically generated notification is displayed.
-        // When the user taps on the notification they are returned to the app. Messages containing both notification
-        // and data payloads are treated as notification messages. The Firebase console always sends notification
-        // messages. For more see: https://firebase.google.com/docs/cloud-messaging/concept-options
-        // [END_EXCLUDE]
 
         Log.d(TAG, "From: " + remoteMessage.getFrom());
 
@@ -65,10 +55,8 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         // Create and show notification
         createNotification(remoteMessage.getNotification().getBody());
 
-        // Also if you intend on generating your own notifications as a result of a received FCM
-        // message, here is where that should be initiated. See sendNotification method below.
     }
-// [END receive_message]
+
 
     /**
      * Create and show a simple notification containing the received FCM message.
@@ -164,26 +152,3 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     }
 
 }
-
-/*
-Log.d(TAG, "yes " + messageBody);
-        Intent intent = new Intent(this, BoardActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code *//*, intent,
-        PendingIntent.FLAG_ONE_SHOT);
-
-        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
-        .setSmallIcon(R.drawable.ic_launcher_background)
-        .setContentTitle("FCM Message")
-        .setContentText(messageBody)
-        .setAutoCancel(true)
-        .setSound(defaultSoundUri)
-        .setContentIntent(pendingIntent);
-        .setVibrate(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400})
-        .setPriority(Notification.PRIORITY_HIGH);
-
-        NotificationManager notificationManager =
-        (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
-        notificationManager.notify(0 /* ID of notification *//*, notificationBuilder.build()); */
