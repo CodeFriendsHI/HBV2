bfac0a00c77788af8796c7b138fd98173faf5e2f
geirgardarsson <geg42@hi.is>
2018-04-11 15:22:02 +0000

    merge from master - missed one

--- KewlKoffee/app/src/main/java/com/example/codefriends/kewlkoffee/BoardActivity.java
+++ KewlKoffee/app/src/main/java/com/example/codefriends/kewlkoffee/BoardActivity.java
@@ -140,7 +140,6 @@ public class BoardActivity extends AppCompatActivity {
             startActivityForResult(intent, NEW_ROOM_CODE);
         });
 
-<<<<<<< HEAD
         FloatingActionButton reload = findViewById(R.id.reloadButton);
         reload.setOnClickListener(v -> requestRooms());
     }
@@ -161,13 +160,11 @@ public class BoardActivity extends AppCompatActivity {
                 System.out.println("Failed to get rooms");
             }
         });
-=======
         //set firebase context
         Firebase.setAndroidContext(this);
 
         notifactionButton = findViewById(R.id.buttonNotify);
         notifactionButton.setOnClickListener(v -> com.example.codefriends.kewlkoffee.Notification.wantKoffeeAll());
->>>>>>> a46cce067de72c39bf060beb48de0ef9f837cd56
     }
 
 
@@ -203,72 +200,4 @@ public class BoardActivity extends AppCompatActivity {
             }
         }
     }
-
-
-    /*private NotificationManager notifManager;
-
-    public void createNotification(String aMessage) {
-        final int NOTIFY_ID = 1002;
-
-        // There are hardcoding only for show it's just strings
-        String name = "my_package_channel";
-        String id = "my_package_channel_1"; // The user-visible name of the channel.
-        String description = "my_package_first_channel"; // The user-visible description of the channel.
-
-        Intent intent;
-        PendingIntent pendingIntent;
-        NotificationCompat.Builder builder;
-
-        if (notifManager == null) {
-            notifManager =
-                    (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
-        }
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            int importance = NotificationManager.IMPORTANCE_HIGH;
-            NotificationChannel mChannel = notifManager.getNotificationChannel(id);
-            if (mChannel == null) {
-                mChannel = new NotificationChannel(id, name, importance);
-                mChannel.setDescription(description);
-                mChannel.enableVibration(true);
-                mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
-                notifManager.createNotificationChannel(mChannel);
-            }
-            builder = new NotificationCompat.Builder(this, id);
-
-            intent = new Intent(this, BoardActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-            pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
-
-            builder.setContentTitle(aMessage)  // required
-                    .setSmallIcon(android.R.drawable.ic_popup_reminder) // required
-                    .setContentText(this.getString(R.string.app_name))  // required
-                    .setDefaults(Notification.DEFAULT_ALL)
-                    .setAutoCancel(true)
-                    .setContentIntent(pendingIntent)
-                    .setTicker(aMessage)
-                    .setVibrate(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
-        } else {
-
-            builder = new NotificationCompat.Builder(this);
-
-            intent = new Intent(this, BoardActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-            pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
-
-            builder.setContentTitle(aMessage)                           // required
-                    .setSmallIcon(android.R.drawable.ic_popup_reminder) // required
-                    .setContentText(this.getString(R.string.app_name))  // required
-                    .setDefaults(Notification.DEFAULT_ALL)
-                    .setAutoCancel(true)
-                    .setContentIntent(pendingIntent)
-                    .setTicker(aMessage)
-                    .setVibrate(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400})
-                    .setPriority(Notification.PRIORITY_HIGH);
-        } // else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-
-        Notification notification = builder.build();
-        notifManager.notify(NOTIFY_ID, notification);
-    }*/
 }
-
